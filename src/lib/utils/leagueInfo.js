9f56056a69f5e825fc6d7e3d79cccdafe22a47e2
GoForJosh <130623212+GoForJosh@users.noreply.github.com>
2023-04-12 21:22:35 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "your_league_id"; // your league ID
-export const leagueName = "your_league_name"; // your league name
+export const leagueID = "916476828959928320"; // your league ID
+export const leagueName = "They Are Who We Thought They Were"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
